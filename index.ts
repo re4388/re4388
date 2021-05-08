bfbfe2d28130c6f7a335de2377c9ebbd0cc5b09e
re4388 <re4388@gmail.com>
2021-05-08 09:32:45 +0800

    migrate to ts

--- index.ts
+++ index.ts
@@ -4,11 +4,8 @@ const md = require('markdown-it')({
   linkify: true, // Autoconvert URL-like text to links
 });
 
-let { graphql } = require('@octokit/graphql');
-
+import * as fs from 'fs';
 const emoji = require('markdown-it-emoji');
-const fs = require('fs');
-const { log } = require('console');
 require('dotenv').config();
 
 const websiteUrl = 'https://ben-notes.vercel.app/#/repos';
@@ -17,18 +14,10 @@ const linkedInUrl = 'https://www.linkedin.com/in/pinweihu/';
 const mediumUrl = 'https://medium.com/@hupinwei';
 const devToUrl = 'https://dev.to/re4388';
 const badgeHeight = '25';
-const repoLimit = 10;
 
 md.use(emoji);
 
 (async () => {
-  // let repo = '';
-  // try {
-  //   repo = await loadLatestRepo();
-  // } catch (e) {
-  //   console.error(`Failed to load repo from github`, e);
-  // }
-
   const twitterBadge = `[<img src="https://img.shields.io/badge/twitter-%231DA1F2.svg?&style=for-the-badge&logo=twitter&logoColor=white" height=${badgeHeight}>](${twitterUrl})`;
   const linkedInBadge = `[<img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" height=${badgeHeight}>](${linkedInUrl})`;
   const mediumBadge = `[<img src="https://img.shields.io/badge/medium-%2312100E.svg?&style=for-the-badge&logo=medium&logoColor=white" height=${badgeHeight}>](${mediumUrl})`;
@@ -44,10 +33,10 @@ md.use(emoji);
   // `;
 
   /* assemble the content ver 2*/
-  text = `👋 Hi, I am Ben Hu, a coder have passion :).
+  let text = `👋 Hi, I am Ben Hu, a coder have passion :).
     \n\n${twitterBadge} ${linkedInBadge}${mediumBadge} ${devToBadge}
     \n\n ❤ [Check out my website](${websiteUrl})
-    \n\n ❤ Today I learned
+    \n\n ❤ Today I learned:
     \n\n
 
     `;
@@ -61,43 +50,3 @@ md.use(emoji);
     console.log(`${result} > README.md`);
   });
 })();
-
-// async function loadLatestRepo() {
-//   const TOKEN = process.env.ACCESS_TOKEN;
-//   graphql = graphql.defaults({
-//     headers: {
-//       authorization: `token ${TOKEN}`,
-//     },
-//   });
-
-//   const query = `{
-//     viewer {
-//       repositories(last: 100, orderBy: {field: CREATED_AT, direction: DESC}) {
-//         edges {
-//           node {
-//             id
-//             name
-//             url
-//             createdAt
-//           }
-//         }
-//       }
-//     }
-//   }`;
-//   const result = await graphql(query);
-//   last10 = result.viewer.repositories.edges;
-//   let links = '';
-//   last10.slice(0, repoLimit).forEach((item) => {
-//     links += `<li><a href=${item.node.url}>${item.node.createdAt.substring(
-//       0,
-//       10
-//     )} - ${item.node.name} </a></li>`;
-//   });
-//   return `<ul>
-//         ${links}
-//       </ul>\n
-
-// :) [More My GitHub History](${websiteUrl})`;
-// }
-
-// console.log(loadLatestRepo())
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
-<p>👋 Hi, I am Ben Hu</p>
+<p>👋 Hi, I am Ben HuXX, a coder have passion 😃.</p>
 <p><a href="https://twitter.com/re4388"><img src="https://img.shields.io/badge/twitter-%231DA1F2.svg?&style=for-the-badge&logo=twitter&logoColor=white" height=25></a> <a href="https://www.linkedin.com/in/pinweihu/"><img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" height=25></a><a href="https://medium.com/@hupinwei"><img src="https://img.shields.io/badge/medium-%2312100E.svg?&style=for-the-badge&logo=medium&logoColor=white" height=25></a> <a href="https://dev.to/re4388"><img src="https://img.shields.io/badge/DEV.TO-%230A0A0A.svg?&style=for-the-badge&logo=dev-dot-to&logoColor=white" height=25></a></p>
 <p>❤ <a href="https://ben-notes.vercel.app/#/repos">Check out my website</a></p>
+<p>❤ Today I learned</p>
