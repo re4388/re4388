caf97a1370826a35da194da11a352f14c7570ed6
re4388 <re4388@gmail.com>
2021-05-23 17:03:03 +0800

    add 2 til v4

--- src/postProcess.ts
+++ src/postProcess.ts
@@ -1,7 +1,6 @@
 import { genTILData } from './genTIL';
 import { Article } from './type';
 import { updateArticleFromGithubTIL } from './updateNotionFromTIL';
-
 const { Client } = require('@notionhq/client');
 require('dotenv').config();
 
@@ -25,31 +24,54 @@ const database_id = '2e9b31c52cba498f8544649a804cce95';
 
     // get current article in this repo, TIL
     const articleInGitHub = genTILData().articleTitleList;
-    const articleInGitHubPath = genTILData().PathToArticleList;
+    const articleInGitHubUrl = genTILData().PathToArticleList;
     const tags = genTILData().tags;
     console.log('articleInGitHub: ', articleInGitHub);
-    console.log('articleInGitHubPath: ', articleInGitHubPath);
+    console.log('articleInGitHubPath: ', articleInGitHubUrl);
     console.log('tags: ', tags);
+
+    // gen all article props
+    const tilList = [];
+    for (let idx = 0; idx < articleInGitHub.length; idx++) {
+      tilList.push({
+        title: articleInGitHub[idx],
+        url: articleInGitHubUrl[idx],
+        tag: tags[idx],
+      });
+    }
 
     // get the interaction set to only update the article
     // that exists in TIL but not exist in Notion
     let gitHubArticleSet = new Set(articleInGitHub);
     let ArticleInNotionSet = new Set(ArticleInNotion);
-    let articleToUpdateToNotion = new Set(
-      [...gitHubArticleSet].filter((x) => !ArticleInNotionSet.has(x))
-    );
+    let articleToUpdateToNotion = [
+      ...new Set(
+        [...gitHubArticleSet].filter((x) => !ArticleInNotionSet.has(x))
+      ),
+    ];
 
     console.log('articleToUpdateToNotion:', articleToUpdateToNotion);
 
+    // also get the url and tag for the intersection set to update
+    const articleInGitHubUrlToNotion = [];
+    const tagsToNotion = [];
+    for (const article of articleToUpdateToNotion) {
+      articleInGitHubUrlToNotion.push(
+        tilList.filter((i) => i.title === article)[0].url
+      );
+      tagsToNotion.push(tilList.filter((i) => i.title === article)[0].tag);
+    }
+
+    // get property
     const { properties } = await notion.databases.retrieve({
       database_id: database_id,
     });
 
     // Update TIL  article to notion_db
     const notionProps = updateArticleFromGithubTIL(
-      Array.from(articleToUpdateToNotion),
-      articleInGitHubPath,
-      tags,
+      articleToUpdateToNotion,
+      articleInGitHubUrlToNotion,
+      tagsToNotion,
       properties
     );
 
