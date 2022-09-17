8c67cfd7201b05ed61704fab1cc91eb39911354f
ben.hu <ben.hu@ert.com>
2022-09-17 17:00:29 +0800

    remove TIL section due to outdated - remove readme again

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,12 @@ const emoji = require('markdown-it-emoji');
 md.use(emoji);
 
 import * as fs from 'fs';
-import { genTILData } from './genTIL';
+// import { genTILData } from './genTIL';
 import { welcomeMessage } from './welcomeMsg';
 require('dotenv').config();
 
-const result = md.render(welcomeMessage + genTILData().allTitleAndUrlInMd);
+// const result = md.render(welcomeMessage + genTILData().allTitleAndUrlInMd);
+const result = md.render(welcomeMessage);
 
 fs.writeFile('README.md', result, function (err) {
   if (err) return console.log(err);
