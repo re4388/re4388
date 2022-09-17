4bab6c76f0bdf7775d3509b9db1baae6649e64d5
ben.hu <ben.hu@ert.com>
2022-09-17 15:56:40 +0800

    remove TIL section due to outdated

--- src/welcomeMsg.ts
+++ src/welcomeMsg.ts
@@ -1,12 +1,13 @@
 const WebsiteUrl = 'https://ben-notes.vercel.app/#/repos';
-const TwitterUrl = 'https://twitter.com/re4388';
+// const TwitterUrl = 'https://twitter.com/re4388';
 const LinkedInUrl = 'https://www.linkedin.com/in/pinweihu/';
 const MediumUrl = 'https://medium.com/@hupinwei';
 const DevToUrl = 'https://dev.to/re4388';
 const badgeHeight = '25';
 
-const TwitterBadge = `[<img src="https://img.shields.io/badge/twitter-%231DA1F2.svg?&\
-    style=for-the-badge&logo=twitter&logoColor=white" height=${badgeHeight}>](${TwitterUrl})`;
+// remove twitter since I seldom use
+// const TwitterBadge = `[<img src="https://img.shields.io/badge/twitter-%231DA1F2.svg?&\
+//     style=for-the-badge&logo=twitter&logoColor=white" height=${badgeHeight}>](${TwitterUrl})`;
 
 const linkedInBadge = `[<img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&\
     style=for-the-badge&logo=linkedin&logoColor=white" height=${badgeHeight}>](${LinkedInUrl})`;
@@ -18,9 +19,8 @@ const DevToBadge = `[<img src="https://img.shields.io/badge/DEV.TO-%230A0A0A.svg
     style=for-the-badge&logo=dev-dot-to&logoColor=white" height=${badgeHeight}>](${DevToUrl})`;
 
 export const welcomeMessage = `👋 Hi, Ben here, you can know me by checking below links!  :).
-    \n\n${TwitterBadge} ${linkedInBadge}${MediumBadge} ${DevToBadge}
     \n\n 😁 [Check out my website](${WebsiteUrl})
+    \n\n ${linkedInBadge}${MediumBadge} ${DevToBadge}
     \n\n
-    \n\n # Today I learned
     \n\n
     `;
